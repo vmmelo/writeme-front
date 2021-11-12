e386011baa02a3bc0071be393ed568772e3bb6f2
Victor Miranda <vmmelo1@gmail.com>
2021-11-12 03:01:36 -0300

    Updates

---
+++ docs/precache-manifest.1688b1148388cc401b51b13e25274a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7727fea4e01af5241666c7db0778232",
+    "url": "/writeme-front/index.html"
+  },
+  {
+    "revision": "bef6dd3a7c3882dcde2d",
+    "url": "/writeme-front/static/css/main.54e08f7f.chunk.css"
+  },
+  {
+    "revision": "dad4bbbe18106b29e5ec",
+    "url": "/writeme-front/static/js/2.220cd550.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/writeme-front/static/js/2.220cd550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef6dd3a7c3882dcde2d",
+    "url": "/writeme-front/static/js/main.b724f55e.chunk.js"
+  },
+  {
+    "revision": "3c5b182ecbeda61d0c05",
+    "url": "/writeme-front/static/js/runtime-main.2705e149.js"
+  }
+]);
