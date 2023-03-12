4b0beee02941d76ae8fa13c2869d6a0bcb217ea2
Victor Miranda <victor.miranda@molacorban.com.br>
2023-03-12 18:11:53 -0300

    Updates

--- precache-manifest.693e3287d5be1ae54437872f4f9d63cf.js
+++ precache-manifest.693e3287d5be1ae54437872f4f9d63cf.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "76f629bc3c84b5dd9b744c36aa5ab998",
+    "revision": "d6b44fb91d6b6f3177680e3e985a6dd5",
     "url": "/writeme-front/index.html"
   },
   {
-    "revision": "da1c881887f32348e99f",
+    "revision": "698c7377f8ba8d50c64d",
     "url": "/writeme-front/static/css/main.96d13d51.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/writeme-front/static/js/2.f4881852.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "da1c881887f32348e99f",
-    "url": "/writeme-front/static/js/main.d6eb6de0.chunk.js"
+    "revision": "698c7377f8ba8d50c64d",
+    "url": "/writeme-front/static/js/main.04fc4425.chunk.js"
   },
   {
     "revision": "9e9de1d4dcb608f8e29e",
